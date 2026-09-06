08823e121ab0e171e5989e225b0ec986727e63e4
NetZen <mycoach808@proton.me>
2026-09-06 19:06:30 +0530

    Core: apply rustfmt rollback error match

--- src/service.rs
+++ src/service.rs
@@ -39,12 +39,13 @@ impl From<AuthoritativeCommandError> for ServiceError {
                 other => Self::Persistence(other),
             },
             AuthoritativeCommandError::InvalidOperation => Self::InvalidInput,
-            AuthoritativeCommandError::RollbackFailure { operation, rollback } => {
-                Self::RollbackFailure {
-                    operation: Box::new(Self::from(*operation)),
-                    rollback,
-                }
-            }
+            AuthoritativeCommandError::RollbackFailure {
+                operation,
+                rollback,
+            } => Self::RollbackFailure {
+                operation: Box::new(Self::from(*operation)),
+                rollback,
+            },
         }
     }
 }
