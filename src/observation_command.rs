aeef2d54c97bbd0b26e8a13b35b5fd500df44e3d
NetZen <mycoach808@proton.me>
2026-09-12 11:16:36 +0530

    style: match existing rollback error formatting

--- src/observation_command.rs
+++ src/observation_command.rs
@@ -34,11 +34,9 @@ impl From<AuthoritativeCommandError> for ObservationCommandError {
                 other => Self::Persistence(other),
             },
             AuthoritativeCommandError::InvalidOperation => Self::InvalidInput,
-            AuthoritativeCommandError::RollbackFailure { operation, rollback } => {
-                Self::RollbackFailure {
-                    operation: Box::new(Self::from(*operation)),
-                    rollback,
-                }
+            AuthoritativeCommandError::RollbackFailure { operation, rollback } => Self::RollbackFailure {
+                operation: Box::new(Self::from(*operation)),
+                rollback,
             },
         }
     }
