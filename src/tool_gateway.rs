28a24ec7628655fa18de5e6913ba6fe18ad6ea0a
NetZen <mycoach808@proton.me>
2026-09-20 13:28:10 +0530

    fix: use repository rustfmt single-line persistence import

--- src/tool_gateway.rs
+++ src/tool_gateway.rs
@@ -6,9 +6,7 @@
 use crate::authorization::{AuthorizationRequest, AuthorizationResult};
 use crate::authorization_enforcement::{validate_authorization, AuthorizationValidationError};
 use crate::capability_lease::{CapabilityLease, CapabilityLeaseError};
-use crate::persistence::{
-    IdempotencyClaim, IdempotencyLifecycleStore, PersistenceError,
-};
+use crate::persistence::{IdempotencyClaim, IdempotencyLifecycleStore, PersistenceError};
 use crate::tool_registry::{
     InMemoryToolRegistry, ToolDataClass, ToolExecutionMode, ToolRegistryEntry, ToolRegistryError,
     ToolVersionRequirement,
