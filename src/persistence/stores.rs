bb7e4922a619a0d4f629775288d44fa4ae0d4356
NetZen <mycoach808@proton.me>
2026-09-22 13:45:44 +0530

    refactor: split persistence contracts by responsibility

---
+++ src/persistence/stores.rs
@@ -0,0 +1,31 @@
+use crate::{Case, EventEnvelope, Id, Incident};
+use super::error::PersistenceError;
+use super::resource::Persisted;
+
+pub trait CaseStore {
+    fn get_case(&self, id: &Id) -> Result<Option<Persisted<Case>>, PersistenceError>;
+    fn create_case(&mut self, value: Persisted<Case>) -> Result<(), PersistenceError>;
+    fn update_case(
+        &mut self,
+        id: &Id,
+        expected_revision: Revision,
+        value: Case,
+    ) -> Result<Revision, PersistenceError>;
+}
+
+pub trait IncidentStore {
+    fn get_incident(&self, id: &Id) -> Result<Option<Persisted<Incident>>, PersistenceError>;
+    fn create_incident(&mut self, value: Persisted<Incident>) -> Result<(), PersistenceError>;
+    fn update_incident(
+        &mut self,
+        id: &Id,
+        expected_revision: Revision,
+        value: Incident,
+    ) -> Result<Revision, PersistenceError>;
+}
+
+pub trait EventStore {
+    fn get_event(&self, id: &Id) -> Result<Option<Persisted<EventEnvelope>>, PersistenceError>;
+    fn append_event(&mut self, value: Persisted<EventEnvelope>) -> Result<(), PersistenceError>;
+}
+
