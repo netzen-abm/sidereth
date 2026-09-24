aceac8934521ad1e28f7bb1774789dcd925df07e
NetZen <mycoach808@proton.me>
2026-09-24 13:40:23 +0530

    fix: keep transaction boundary independent

--- src/persistence/transaction.rs
+++ src/persistence/transaction.rs
@@ -1,6 +1,4 @@
 use super::error::PersistenceError;
-use super::idempotency::IdempotencyClaim;
-use crate::Id;
 
 pub trait Transaction {
     fn commit(self) -> Result<(), PersistenceError>;
@@ -11,9 +9,4 @@ pub trait TransactionFactory {
     type Tx: Transaction;
 
     fn begin(&mut self) -> Result<Self::Tx, PersistenceError>;
-}
-
-pub trait IdempotencyStore {
-    fn lookup(&self, operation_id: &Id) -> Result<bool, PersistenceError>;
-    fn claim(&mut self, operation_id: Id) -> Result<IdempotencyClaim, PersistenceError>;
 }
